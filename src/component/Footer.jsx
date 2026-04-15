5d787a75a1d505f320235a82654951af1d2777fe
mdihbinah <mdihbinah@gmail.com>
2026-04-15 11:49:18 +0600

    Set up clear and start code.

---
+++ src/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="text-5xl italic">I am footer</div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,11 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="text-3xl">Hello</div>
+            <button className="btn btn-accent">hkdkd</button>
+        </div>
+    );
+};
+
+export default Navbar;
